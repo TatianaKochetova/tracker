42452d8adead6aad3c284d5aaa7e60ab23e6e663
TatianaKochetova <t.kochetova514@gmail.com>
2020-03-16 13:09:52 -0400

    Заполнение массива [226863]

--- Chapter_001/src/main/java/array/ArrayDefinition.java
+++ Chapter_001/src/main/java/array/ArrayDefinition.java
@@ -5,5 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        {
+            names[0] = "Madonna";
+            names[1] = "Rhianna";
+            names[2] = "Sting";
+            names[3] = "Stellar";
+
+        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+
+
     }
 }
