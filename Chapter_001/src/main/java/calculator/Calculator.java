8b64a73798560efa0670a70031d4753871a880d9
TatianaKochetova <t.kochetova514@gmail.com>
2020-03-16 12:37:32 -0400

    Java Doc. Code convention. [226919]

--- Chapter_001/src/main/java/calculator/Calculator.java
+++ Chapter_001/src/main/java/calculator/Calculator.java
@@ -1,5 +1,8 @@
-package ru.job4j.calculator;
-
+package calculator;
+/**
+ * Calculator.
+ * @author Kochetova Tatiana (t.kochetova514@gmail.com)
+ */
 public class Calculator {
     public static void main(String[] args) {
         int one = 1;
@@ -7,10 +10,25 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
+        /**
+         * метод, выполняющий сложение
+         */
         int onePlusTwo = one + two;
-        int sixDivTwo = 6 / 2;
-        int fiveMinusTwo = 5-2;
-        int fourTimeTwo = 4 * 2;
+        /**
+         * метод, выполняющий деление
+         */
+        int sixDivTwo = six / two;
+        /**
+         * метод, выполняющий вычитание
+         */
+        int fiveMinusTwo = five - two;
+        /**
+         * метод, выполняющий умножение
+         */
+        int fourTimeTwo = four *two;
+        /**
+         * вывод на консоль
+         */
         System.out.println(onePlusTwo);
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
