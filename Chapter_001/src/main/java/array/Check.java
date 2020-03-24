7a31fd95ca8810d9e67e5baff6e60ccd43343539
TatianaKochetova <t.kochetova514@gmail.com>
2020-03-24 13:10:28 -0400

    Массив заполнен true или false. [226848]

---
+++ Chapter_001/src/main/java/array/Check.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++)
+            if (data[0] != data[i]) {
+                result = false;
+                break;
+            }
+        return result;
+    }
+
+
+
+}
